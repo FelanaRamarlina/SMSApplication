c79c2b98adec1b22d2740356b13b3c760ab8e190
Felana <felana.r@hotmail.fr>
2018-04-11 19:55:38 +0200

    exercice 2

--- app/src/main/java/com/example/framarlina/smsapplication/MainActivity.java
+++ app/src/main/java/com/example/framarlina/smsapplication/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.framarlina.smsapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Button;
+
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,9 +20,23 @@ public class MainActivity extends AppCompatActivity {
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        TextView text = new TextView(this);
+        /*TextView text = new TextView(this);
         text.setText("Hello Baby World!&&&&&");
+        setContentView(R.layout.activity_main);*/
         setContentView(R.layout.activity_main);
+        final EditText edit = (EditText) findViewById(R.id.editText);
+        final TextView viewtest = (TextView) findViewById(R.id.textView);
+        final Button btn = (Button) findViewById(R.id.showBtn);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                viewtest.setText(edit.getText());
+            }
+        });
+
     }
+
+
 
 }
